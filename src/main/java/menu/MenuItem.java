6b9195f3a4f65d45adc0edafc34399992b64c0a8
cheolhyeon <tghy4@naver.com>
2023-11-11 22:18:53 +0900

    feat : 메뉴 객체의 상태 정보를 관리할 ENUM 클래스 구현

---
+++ src/main/java/menu/MenuItem.java
@@ -0,0 +1,35 @@
+package menu;
+
+public enum MenuItem {
+    MUSHROOM_SOUP("양송이수프",6000,"에피타이저"),
+    TAPAS("타파스", 5500,"에피타이저"),
+    CAESAR_SALAD("시저샐러드", 8000,"에피타이저"),
+    T_BONE_STEAK("티본스테이크", 55000, "메인"),
+    BBQ_RIBS("바비큐립", 54000, "메인"),
+    SEAFOOD_PASTA("해산물파스타", 35000, "메인"),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000, "메인"),
+    CHOCOLATE_CAKE("초코케이크", 15000, "디저트"),
+    ICE_CREAM("아이스크림", 5000, "디저트"),
+    ZERO_COLA("제로콜라", 3000, "음료"),
+    RED_WINE("레드와인", 60000, "음료"),
+    CHAMPAGNE("샴페인", 25000, "음료");
+    private String menuName;
+    private int price;
+    private String menuCategory;
+    MenuItem(String menuName, int price, String menuCategory) {
+        this.menuName = menuName;
+        this.price = price;
+        this.menuCategory = menuCategory;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    public String getMenuCategory() {
+        return menuCategory;
+    }
+}
