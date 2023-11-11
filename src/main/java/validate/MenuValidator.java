1baab8b99573535f38d9e41d0857fc669ee78878
cheolhyeon <tghy4@naver.com>
2023-11-11 17:57:42 +0900

    feat : 파라미터로 들어온 수량을 정수로 파싱해서 검증하는 기능 구현 완료

--- src/main/java/validate/MenuValidator.java
+++ src/main/java/validate/MenuValidator.java
@@ -9,5 +9,13 @@ public abstract class MenuValidator {
         if (quantity <= 0) {
             throw new IllegalArgumentException("[ERROR] 주문 수량은 1개 이상이여야 합니다.");
         }
+    }   public static int validateConvertToInt(String input) {
+        int quantity;
+        try {
+            quantity = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("[ERROR] 수량은 숫자로만 입력해주세요");
+        }
+        return quantity;
     }
 }
