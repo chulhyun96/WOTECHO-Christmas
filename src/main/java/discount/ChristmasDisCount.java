b41678a5bb7a3fd731d9c7894c280af729693e3c
cheolhyeon <tghy4@naver.com>
2023-11-12 15:19:51 +0900

    refactor :  코드 구조 개선

--- src/main/java/discount/ChristmasDisCount.java
+++ src/main/java/discount/ChristmasDisCount.java
@@ -6,16 +6,24 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public abstract class ChristmasDisCount {
-    private static final int DISCOUNT_PRICE = 1000;
+    private static final int DISCOUNT_DEFAULT_PRICE = 1000;
     private static final int DISCOUNT_IMPEDING_ON_DATE = 100;
+    private static final String CHRISTMAS_DISCOUNT_MESSAGE = CHRISTMAS_DDAY.getName()+": -%,d원";
+    private static final String NONE_DISCOUNT = "0";
 
     public static String christmasDiscount(LocalDate inputDate) {
         if (!inputDate.isBefore(CHRISTMAS_DDAY.getStartDate()) &&
                 !inputDate.isAfter(CHRISTMAS_DDAY.getEndDate())) {
-            long date = ChronoUnit.DAYS.between(CHRISTMAS_DDAY.getStartDate(), inputDate);
-            int discount = DISCOUNT_PRICE + ((int) date * DISCOUNT_IMPEDING_ON_DATE);
-            return "-" + discount;
+
+            int discount = getDiscount(inputDate);
+
+            return String.format(CHRISTMAS_DISCOUNT_MESSAGE, discount);
         }
-        return "0";
+        return NONE_DISCOUNT;
+    }
+
+    private static int getDiscount(LocalDate inputDate) {
+        long date = ChronoUnit.DAYS.between(CHRISTMAS_DDAY.getStartDate(), inputDate);
+        return DISCOUNT_DEFAULT_PRICE + ((int) date * DISCOUNT_IMPEDING_ON_DATE);
     }
 }
