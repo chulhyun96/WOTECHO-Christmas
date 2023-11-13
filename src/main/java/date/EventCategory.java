3db553719e933248b68accc915e4fbe5d00636b3
cheolhyeon <tghy4@naver.com>
2023-11-13 19:51:44 +0900

    refactor : 코드 구조 개선

---
+++ src/main/java/date/EventCategory.java
@@ -0,0 +1,19 @@
+package date;
+
+public enum EventCategory {
+    CHRISTMAS("크리스마스 디데이 할인"),
+    WEEKDAY("평일 할인"),
+    WEEKEND("주말 할인"),
+    SPECIAL("특별 할인"),
+    GIFT("증정 이벤트");
+
+    private String eventName;
+
+    EventCategory(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+}
