bccd76dc89b60473dddb9177905ff648a4779dda
cheolhyeon <tghy4@naver.com>
2023-11-14 16:37:34 +0900

    feat : 이벤트 배지 부여 클래스 구현

---
+++ src/main/java/gift/BadgeGiftEvent.java
@@ -0,0 +1,30 @@
+package gift;
+
+import java.util.List;
+import order.OrderInfo;
+
+public abstract class BadgeGiftEvent {
+
+    public String checkGiftBadge(List<OrderInfo>orderInfoList) {
+
+        int allPrice = getAllPrice(orderInfoList);
+
+        if (Badge.STAR.getBadgeValue() <= allPrice && Badge.TREE.getBadgeValue() > allPrice) {
+            return Badge.STAR.getBadgeName();
+        }
+        if (Badge.TREE.getBadgeValue() <= allPrice && Badge.SANTA.getBadgeValue() > allPrice) {
+            return Badge.TREE.getBadgeName();
+        }
+        if (Badge.SANTA.getBadgeValue() <= allPrice) {
+            return Badge.SANTA.getBadgeName();
+        }
+        return null;
+    }
+
+    private static int getAllPrice(List<OrderInfo> orderInfoList) {
+        int allPrice = orderInfoList.stream()
+                .mapToInt(OrderInfo::getAllPrice)
+                .sum();
+        return allPrice;
+    }
+}
