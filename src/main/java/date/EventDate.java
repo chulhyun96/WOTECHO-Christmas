240dd8d7157f4f3520ab063db26bf00b9d5c9dbd
cheolhyeon <tghy4@naver.com>
2023-11-14 16:09:22 +0900

    refactor :  코드 구조 개선

--- src/main/java/date/EventDate.java
+++ src/main/java/date/EventDate.java
@@ -3,31 +3,15 @@ package date;
 import java.time.LocalDate;
 
 public enum EventDate {
-    CHRISTMAS_DDAY("????? ??? ??", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 25)),
-    WEEKDAY_DISCOUNT("?? ??", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31)),
-    WEEKEND_DISCOUNT("?? ??", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31)),
-    SPECIAL_DISCOUNT("?? ??", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31)),
-    GIFT_EVENT("?? ???", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31));
-
-    private String name;
-    private LocalDate startDate;
-    private LocalDate endDate;
-    EventDate(String name, LocalDate startDate, LocalDate endDate) {
-        this.name = name;
-        this.startDate = startDate;
-        this.endDate = endDate;
+    EVENT_START(LocalDate.of(2023, 12, 1)),
+    EVENT_END(LocalDate.of(2023, 12, 31)),
+    CHRISTMAS_START(LocalDate.of(2023, 12, 1)),
+    CHRISTMAS_END(LocalDate.of(2023, 12, 25));
+    private final LocalDate date;
+    EventDate(LocalDate date) {
+        this.date = date;
     }
-
-    public String getName() {
-        return name;
+    public LocalDate getDate() {
+        return date;
     }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
 }
