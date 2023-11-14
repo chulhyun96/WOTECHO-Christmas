2a862c9be759e709453f69005cee1261a9b296d0
cheolhyeon <tghy4@naver.com>
2023-11-14 16:08:54 +0900

    refactor :  코드 구조 개선

--- src/main/java/discount/ChristmasDisCount.java
+++ src/main/java/discount/ChristmasDisCount.java
@@ -1,26 +1,32 @@
 package discount;
 
-import static date.EventDate.*;
+import static constants.SystemConstants.NONE_DISCOUNT;
+import static date.Event.CHRISTMAS_DDAY;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
+import order.OrderInfo;
+import validate.EventCaution;
 
 public abstract class ChristmasDisCount {
     private static final int DISCOUNT_DEFAULT_PRICE = 1000;
     private static final int DISCOUNT_IMPEDING_ON_DATE = 100;
-    private static final String CHRISTMAS_DISCOUNT_MESSAGE = CHRISTMAS_DDAY.getName()+": -%,d원";
-    private static final String NONE_DISCOUNT = "0";
+    private static final String CHRISTMAS_DISCOUNT_MESSAGE = CHRISTMAS_DDAY.getDisplayName() + ": -%,d원";
 
-    public static String christmasDiscount(LocalDate inputDate) {
-        if (!inputDate.isBefore(CHRISTMAS_DDAY.getStartDate()) &&
-                !inputDate.isAfter(CHRISTMAS_DDAY.getEndDate())) {
 
-            int discount = getDiscount(inputDate);
+    public static String christmasDiscount(List<OrderInfo> orderInfoList, LocalDate inputDate) {
+        if (EventCaution.checkIfEventAcceptPrice(orderInfoList)) {
+            if (!inputDate.isBefore(CHRISTMAS_DDAY.getStartDate()) &&
+                    !inputDate.isAfter(CHRISTMAS_DDAY.getEndDate())) {
 
-            return String.format(CHRISTMAS_DISCOUNT_MESSAGE, discount);
+                int discount = getDiscount(inputDate);
+                return String.format(CHRISTMAS_DISCOUNT_MESSAGE, discount);
+            }
         }
         return NONE_DISCOUNT;
     }
+
 
     private static int getDiscount(LocalDate inputDate) {
         long date = ChronoUnit.DAYS.between(CHRISTMAS_DDAY.getStartDate(), inputDate);
