996b695bd0f2cb78acaac2c44af658f272f52edd
cheolhyeon <tghy4@naver.com>
2023-11-15 16:20:42 +0900

    refactor : 코드 구조 개선

--- src/main/java/menu/MenuItem.java
+++ src/main/java/menu/MenuItem.java
@@ -1,22 +1,24 @@
 package menu;
 
+import static menu.MenuCategory.*;
+
 public enum MenuItem {
-    MUSHROOM_SOUP("양송이수프",6000,"에피타이저"),
-    TAPAS("타파스", 5500,"에피타이저"),
-    CAESAR_SALAD("시저샐러드", 8000,"에피타이저"),
-    T_BONE_STEAK("티본스테이크", 55000, "메인"),
-    BBQ_RIBS("바비큐립", 54000, "메인"),
-    SEAFOOD_PASTA("해산물파스타", 35000, "메인"),
-    CHRISTMAS_PASTA("크리스마스파스타", 25000, "메인"),
-    CHOCOLATE_CAKE("초코케이크", 15000, "디저트"),
-    ICE_CREAM("아이스크림", 5000, "디저트"),
-    ZERO_COLA("제로콜라", 3000, "음료"),
-    RED_WINE("레드와인", 60000, "음료"),
-    CHAMPAGNE("샴페인", 25000, "음료");
+    MUSHROOM_SOUP("양송이수프",6000, APPETIZER),
+    TAPAS("타파스", 5500, APPETIZER),
+    CAESAR_SALAD("시저샐러드", 8000, APPETIZER),
+    T_BONE_STEAK("티본스테이크", 55000, MAIN),
+    BBQ_RIBS("바비큐립", 54000, MAIN),
+    SEAFOOD_PASTA("해산물파스타", 35000, MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000, MAIN),
+    CHOCOLATE_CAKE("초코케이크", 15000, DESSERT),
+    ICE_CREAM("아이스크림", 5000, DESSERT),
+    ZERO_COLA("제로콜라", 3000, BEVERAGE),
+    RED_WINE("레드와인", 60000, BEVERAGE),
+    CHAMPAGNE("샴페인", 25000, BEVERAGE);
     private String menuName;
     private int price;
-    private String menuCategory;
-    MenuItem(String menuName, int price, String menuCategory) {
+    private final MenuCategory menuCategory;
+    MenuItem(String menuName, int price, MenuCategory menuCategory) {
         this.menuName = menuName;
         this.price = price;
         this.menuCategory = menuCategory;
@@ -29,7 +31,16 @@ public enum MenuItem {
     public int getPrice() {
         return price;
     }
-    public String getMenuCategory() {
+
+    public MenuCategory getMenuCategory() {
         return menuCategory;
+    }
+    public static MenuItem findMenuItemByName(String menuName) {
+        for (MenuItem item : MenuItem.values()) {
+            if (item.getMenuName().equalsIgnoreCase(menuName)) {
+                return item;
+            }
+        }
+        return null;
     }
 }
