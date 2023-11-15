b25ad8afdb6267eb780f8ab3a8971cd409083600
cheolhyeon <tghy4@naver.com>
2023-11-15 16:19:56 +0900

    refactor : 코드 구조 개선

--- src/main/java/validate/EventCaution.java
+++ src/main/java/validate/EventCaution.java
@@ -8,14 +8,17 @@ import order.OrderInfo;
 
 public abstract class EventCaution {
     private static final int UNACCEPTABLE_PRICE = 10000;
+    private static final int OVER_ORDER_COUNT = 20;
+    private static final String IF_ONLY_ORDER_BEVERAGE = "[ERROR] 음료만 주문할 수 없습니다.";
+    private static final String IF_ORDER_COUNT_OVER_MAX = "[ERROR] 메뉴는 한번에 최대 20개 까지만 주문할 수 있습니다.";
 
     public static void checkIfAllBeverages(List<String> menuNames) {
         boolean allBeverages = isAllBeverages(menuNames);
         if (allBeverages) {
-            throw new IllegalArgumentException("[ERROR] 음료만 주문할 수 없습니다.");
+            throw new IllegalArgumentException(IF_ONLY_ORDER_BEVERAGE);
         }
     }
-    public static boolean checkIfEventAcceptPrice(List<OrderInfo> orderInfoList) {
+    public static boolean checkIfEventAccept(List<OrderInfo> orderInfoList) {
         int allPrice = 0;
         for (OrderInfo orderInfo : orderInfoList) {
             allPrice += orderInfo.getMenuPrice() * orderInfo.getQuantity();
@@ -26,8 +29,8 @@ public abstract class EventCaution {
         return false;
     }
     public static void checkMenuCounts(List<OrderInfo> orderInfoList) {
-        if (orderInfoList.size() > 20) {
-            throw  new IllegalArgumentException("[ERROR] 메뉴는 한번에 최대 20개 까지만 주문할 수 있습니다.");
+        if (orderInfoList.size() > OVER_ORDER_COUNT) {
+            throw  new IllegalArgumentException(IF_ORDER_COUNT_OVER_MAX);
         }
     }
 
