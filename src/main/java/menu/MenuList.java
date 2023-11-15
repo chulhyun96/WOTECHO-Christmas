27808679f6a6f459ef480caaa6c8d3951e35e444
cheolhyeon <tghy4@naver.com>
2023-11-15 16:21:06 +0900

    refactor : 코드 구조 개선

--- src/main/java/menu/MenuList.java
+++ src/main/java/menu/MenuList.java
@@ -2,16 +2,17 @@ package menu;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public abstract class MenuList {
-    private static final List<Menu> menuList = createAllMenu();
 
-    public static List<Menu> createAllMenu() {
-        return Stream.of(MenuItem.values())
+
+    private static List<Menu> menuList = new ArrayList<>();
+
+    public static void createAllMenu() {
+        menuList = Stream.of(MenuItem.values())
                 .map(item -> Menu.createMenu(item.getMenuName(), item.getPrice(), item.getMenuCategory()))
-                .collect(Collectors.toList());
+                .toList();
     }
 
     public static List<Menu> getMenuList() {
