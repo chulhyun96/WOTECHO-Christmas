242e01e7343d047eaea14314c3c2fa091c943037
cheolhyeon <tghy4@naver.com>
2023-11-24 09:41:58 +0900

    refactor : 코드 구조 개선 및 예외 테스트 모두 통과,

--- src/main/java/validate/DateValidator.java
+++ src/main/java/validate/DateValidator.java
@@ -6,17 +6,16 @@ public abstract class DateValidator {
     private static final String ERROR_INVALID_DATE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
     public static void validateDateRange(int date) {
-        if (date < INVALID_MIN_DATE || date > INVALID_MAX_DATE) {
+        if (INVALID_MIN_DATE > date || date > INVALID_MAX_DATE ) {
             throw new IllegalArgumentException(ERROR_INVALID_DATE_MESSAGE);
         }
     }
+
     public static int validateDateInput(String dateInput) {
-        int day;
         try {
-            day = Integer.parseInt(dateInput);
+            return Integer.parseInt(dateInput);
         } catch (NumberFormatException e) {
             throw new NumberFormatException(ERROR_INVALID_DATE_MESSAGE);
         }
-        return day;
     }
 }
