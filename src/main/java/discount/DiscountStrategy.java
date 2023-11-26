6e5fa65e30e152b5d515d242f79d19896fa55255
cheolhyeon <tghy4@naver.com>
2023-11-26 17:37:18 +0900

    refactor : 코드 구조 개선

---
+++ src/main/java/discount/DiscountStrategy.java
@@ -0,0 +1,10 @@
+package discount;
+
+import java.time.LocalDate;
+import java.util.List;
+import order.OrderInfo;
+
+public interface DiscountStrategy {
+    int applyDiscountStrategy(List<OrderInfo> orderInfoList, LocalDate localDate);
+    boolean isDiscountAccept(LocalDate localDate);
+}
