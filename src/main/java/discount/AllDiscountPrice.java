ce2e92b7895695340ba990b34e0512b4302eea8a
cheolhyeon <tghy4@naver.com>
2023-11-29 12:16:46 +0900

    refactor : getDisocuntStrategy 메서드의 반복되는 작업을 List로 묶어서 처리하도록 리팩토링

--- src/main/java/discount/AllDiscountPrice.java
+++ src/main/java/discount/AllDiscountPrice.java
@@ -1,34 +1,35 @@
 package discount;
 
+import static constants.SystemConstants.NONE_DISCOUNT;
+
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import order.OrderInfo;
 
 public class AllDiscountPrice {
-    private boolean isStrategyUpdate = false;
-    private int christmasDiscountAmount;
-    private int specialDiscountAmount;
-    private int weekdayDiscountAmount;
-    private int weekendDiscountAmount;
+    private boolean isStrategyUpdate = true;
+    private List<DiscountStrategy> discountStrategyList;
 
-    private void getDiscountStrategy(List<OrderInfo> orderInfoList, LocalDate localDate) {
-        ChristmasDisCount christmasDisCount = ChristmasDisCount.getInstance();
-        this.christmasDiscountAmount = christmasDisCount.applyDiscountStrategy(orderInfoList, localDate);
-
-        SpecialDiscount specialDiscount = SpecialDiscount.getInstance();
-        this.specialDiscountAmount = specialDiscount.applyDiscountStrategy(orderInfoList, localDate);
-
-        WeekdayDiscount weekdayDiscount = WeekdayDiscount.getInstance();
-        this.weekdayDiscountAmount = weekdayDiscount.applyDiscountStrategy(orderInfoList, localDate);
-
-        WeekendDiscount weekendDiscount = WeekendDiscount.getInstance();
-        this.weekendDiscountAmount = weekendDiscount.applyDiscountStrategy(orderInfoList, localDate);
+    private void getDiscountStrategyList() {
+        discountStrategyList = Arrays.asList(ChristmasDisCount.getInstance(), SpecialDiscount.getInstance(),
+                WeekdayDiscount.getInstance(), WeekendDiscount.getInstance());
+    }
+    private boolean isListDuplication() {
+        if (isStrategyUpdate) {
+            getDiscountStrategyList();
+            this.isStrategyUpdate = false;
+            return false;
+        }
+        return true;
     }
     public int getAllDiscountPrice(List<OrderInfo> orderInfoList, LocalDate localDate) {
-        if (!isStrategyUpdate) {
-            getDiscountStrategy(orderInfoList, localDate);
-            this.isStrategyUpdate = true;
+        if (!isListDuplication()) {
+            return discountStrategyList.stream()
+                    .mapToInt(discountStrategyList1 ->
+                            discountStrategyList1.applyDiscountStrategy(orderInfoList, localDate))
+                    .sum();
         }
-        return christmasDiscountAmount + specialDiscountAmount + weekdayDiscountAmount + weekendDiscountAmount;
+        return NONE_DISCOUNT;
     }
 }
